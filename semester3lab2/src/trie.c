f6723563b362e2231409ea480534b72978dd4cfc
Boyvalenko Nikita <n.boywalenko@yandex.ru>
2023-11-27 19:53:57 +0700

    Delete comments

--- semester3lab2/src/trie.c
+++ semester3lab2/src/trie.c
@@ -28,7 +28,7 @@ Trie *trie_lookup(Trie *root, char *key)
         else
             return NULL;
     }
-    return node; /* Node must be a leaf */
+    return node;
 }
 Trie *trie_insert(Trie *root, char *key, int value)
 {
@@ -37,14 +37,13 @@ Trie *trie_insert(Trie *root, char *key, int value)
     list = root;
     for (; *key != '\0'; key++)
     {
-        /* Lookup sibling node */
         for (node = list; node != NULL; node = node->sibling)
         {
             if (node->ch == *key)
                 break;
         }
         if (node == NULL)
-        { /* Node not found. Add new node */
+        {
             node = trie_create();
             node->ch = *key;
             node->sibling = list;
@@ -55,28 +54,28 @@ Trie *trie_insert(Trie *root, char *key, int value)
             list = NULL;
         }
         else
-        { /* Node found. Move to next level */
+        {
             list = node->child;
         }
         parent = node;
     }
-    /* Update value in leaf */
-    // if (node->value != NULL)
-    //     free(node->value);
+
     node->value = value;
     return root;
 }
 Trie *trie_delete(Trie *root, char *key)
 {
     int found;
-    return trie_delete_dfs(root, NULL, key, &found);
+    return trie_delete_node(root, NULL, key, &found);
 }
-Trie *trie_delete_dfs(Trie *root, Trie *parent, char *key, int *found)
+Trie *trie_delete_node(Trie *root, Trie *parent, char *key, int *found)
 {
     Trie *node, *prev = NULL;
     *found = (*key == '\0' && root == NULL) ? 1 : 0;
+
     if (root == NULL || *key == '\0')
         return root;
+
     for (node = root; node != NULL; node = node->sibling)
     {
         if (node->ch == *key)
@@ -85,10 +84,11 @@ Trie *trie_delete_dfs(Trie *root, Trie *parent, char *key, int *found)
     }
     if (node == NULL)
         return root;
-    trie_delete_dfs(node->child, node, key + 1, found);
+
+    trie_delete_node(node->child, node, key + 1, found);
+
     if (*found > 0 && node->child == NULL)
     {
-        /* Delete node */
         if (prev != NULL)
             prev->sibling = node->sibling;
         else
@@ -98,7 +98,6 @@ Trie *trie_delete_dfs(Trie *root, Trie *parent, char *key, int *found)
             else
                 root = node->sibling;
         }
-        // free(node->value);
         free(node);
     }
     return root;
@@ -106,21 +105,21 @@ Trie *trie_delete_dfs(Trie *root, Trie *parent, char *key, int *found)
 void trie_print(Trie *trie, int level)
 {
     for (Trie *node = trie; node != NULL; node = node->sibling)
-    { // loop on all siblings at this level
+    {
         for (int i = 0; i < level; i++)
         {
-            printf(" "); // tab for each level
+            printf(" ");
         }
         if (node->value != NULL)
-        { // end of the key
+        {
             printf("%c ($) (value = %d)\n", node->ch, node->value);
         }
         else
         {
-            printf("%c \n", node->ch); // print char for this level
+            printf("%c \n", node->ch);
         }
         if (node->child != NULL)
-        { // go to the next level
+        {
             trie_print(node->child, level + 1);
         }
     }
--- semester3lab2/src/trie.h
+++ semester3lab2/src/trie.h
@@ -5,11 +5,10 @@ typedef struct trie
     int value;
     struct trie *sibling;
     struct trie *child;
-    // TrieNode* children[N];
 } Trie;
 Trie *trie_create();
 Trie *trie_insert(Trie *root, char *key, int value);
 Trie *trie_lookup(Trie *root, char *key);
 Trie *trie_delete(Trie *root, char *key);
-Trie *trie_delete_dfs(Trie *root, Trie *parent, char *key, int *found);
+Trie *trie_delete_node(Trie *root, Trie *parent, char *key, int *found);
 void trie_print(Trie *root, int level);
